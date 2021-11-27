30e7a0b047b0f178b5fb3c7daab8c7bdb65efc59
shinbunbun <takamuneyuto@gmail.com>
2021-11-27 18:22:31 +0900

    complete

--- handler.js
+++ handler.js
@@ -1,18 +1,33 @@
-'use strict';
+const { TwitterApi } = require('twitter-api-v2');
 
-module.exports.hello = async (event) => {
+const createTweetMessage = () => {
+  const i = Math.floor(Math.random() * (140 - 1 + 1) + 1);
+  let baseMessage = 'まつもとせんぱ';
+  for (let j = 0; j < i; j += 1) {
+    baseMessage += 'ー';
+  }
+  baseMessage += 'い';
+  return baseMessage;
+};
+
+module.exports.hello = async (/* event */) => {
+  const client = new TwitterApi(
+    {
+      appKey: process.env.TWITTER_CONSUMER_KEY,
+      appSecret: process.env.TWITTER_CONSUMER_SECRET,
+      accessToken: process.env.TWITTER_ACCESS_TOKEN,
+      accessSecret: process.env.TWITTER_ACCESS_SECRET,
+    },
+  );
+  const { data: createdTweet } = await client.v2.tweet(createTweetMessage());
+  console.log('Tweet', createdTweet.id, ':', createdTweet.text);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'Tweet succeed!',
       },
-      null,
-      2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
